fcc9083e098f4b324066541bcc59d15c38c2b5c0
Kirill Bobyrev <kbobyrev@google.com>
2021-01-27 14:18:08 +0100

    Create Chrome indexer

---
+++ indexer/run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#===-- run.sh -------------------------------------------------------------===//
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===----------------------------------------------------------------------===//
+#
+# Produce Chromium index.
+#
+#===----------------------------------------------------------------------===//
+
+set -eu
+
+echo "Downloading clangd indexer"
+
+python3 download_latest_release_assets.py --output-name clangd-indexing-tools.zip  --asset-prefix clangd-indexing-tools-linux
+
+unzip clangd-indexing-tools.zip
+
+export CLANGD_INDEXER=$(find . -name 'clangd-indexer' | xargs readlink -f)
+
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+
+export PATH="$PATH:$(readlink -f depot_tools)"
+
+mkdir chromium
+cd chromium
+
+gclient metrics --opt-out
+
+fetch --nohooks chromium
+
+cd src
+
+# Remove snapcraft from dependency list: installing it is not feasible inside
+# Docker.
+sed -i '/if package_exists snapcraft/,/fi/d' ./build/install-build-deps.sh
+./build/install-build-deps.sh
+
+gclient runhooks
+
+gn gen out/Default
+
+ninja -C out/Default/ -t targets all | grep -i '^gen/' | grep -E "\.(cpp|h|inc|cc)\:" | cut -d':' -f1 | xargs autoninja -C out/Default
+
+tools/clang/scripts/generate_compdb.py -p out/Default > compile_commands.json
+
+$CLANGD_INDEXER --executor=all-TUs compile_commands.json > /chrome.idx
+
+7z a chrome-index-$(date -u +%Y%m%d).zip /chrome.idx
+
+CURRENT_COMMIT=$(git rev-parse --short HEAD)
+
+gh release create --repo clangd/chrome-remote-index --title="Index at $(date -u +%Y-%m-%d)" --notes="Index with Default config options at $CURRENT_COMMIT commit." $CURRENT_COMMIT chrome-index-$(date -u +%Y%m%d).zip
