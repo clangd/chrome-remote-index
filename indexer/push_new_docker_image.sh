3631b5ec8225188ae09a043eed0c02a6d2bd8e5b
Kadir Cetinkaya <kadircet@google.com>
2022-01-26 10:30:30 +0100

    Add deployment scripts and documentation for the indexer

---
+++ indexer/push_new_docker_image.sh
@@ -0,0 +1,28 @@
+source args.sh
+
+set -ex
+
+TOKEN="$1"
+if [ -z "$TOKEN" ]; then \
+  echo "Usage: $0 GITHUB_TOKEN_FOR_UPLOADING_INDEX"; \
+  exit -1; \
+fi;
+
+TEMP_DIR="$(mktemp -d)"
+# Make sure we delete TEMP_DIR on exit.
+trap "rm -r $TEMP_DIR" EXIT
+
+# Copy all the necessary files for docker image into a temp directory and move
+# into it.
+cp Dockerfile "$TEMP_DIR/"
+cp cronjob "$TEMP_DIR/"
+cp run.sh "$TEMP_DIR/"
+cp ../download_latest_release_assets.py "$TEMP_DIR/"
+cp prepare.sh "$TEMP_DIR/"
+cp entry_point.sh "$TEMP_DIR/"
+cd "$TEMP_DIR"
+
+# Build the image, tag it for GCR and push.
+docker build --build-arg TOKEN="$TOKEN" -t "$IMAGE_IN_GCR" .
+gcloud auth configure-docker
+docker push "$IMAGE_IN_GCR"
